58e7d3a2312e4a012e33ae2312e7dbb018ee48fb
KiranEarle <earlekiran@gmail.com>
2018-09-13 11:42:18 +0100

    completed task

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './src/client/index.js',
 
   output: {
     path: path.resolve(__dirname, 'assets'),
